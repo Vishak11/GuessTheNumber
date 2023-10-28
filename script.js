6652303dc6b301de5919adb1c67c2f04ea892457
Vishak11 <vishakvn007@gmail.com>
2023-10-29 01:02:39 +0530

    v4

--- script.js
+++ script.js
@@ -1,71 +1,65 @@
 'use strict'
- let Snumber=Math.trunc(Math.random()*20)+1
+let Snumber = Math.trunc(Math.random() * 20) + 1
 console.log(Snumber)
-document.querySelector('.number').textContent=Snumber
-let Score=20
-let OldScore=localStorage.getItem('HighScore',Score)
-document.querySelector('.HighScore').textContent=OldScore
+document.querySelector('.number').textContent = "Guess me"
+let Score = 20
+let OldScore = localStorage.getItem('HighScore', Score)
+document.querySelector('.HighScore').textContent = OldScore
 document.querySelector('.check').addEventListener('click',
 
-function()
-{
-    const guess= Number(document.querySelector('.guess').value)
-    console.log("guess",guess)
-    if(!guess)
-        {
-        document.querySelector('.message').textContent="Empty value"
+    function () {
+        const guess = Number(document.querySelector('.guess').value)
+        console.log("guess", guess)
+        if (!guess) {
+            document.querySelector('.message').textContent = "Empty value"
         }
-    else if(guess==Snumber)
-        {        
-            document.querySelector('.message').textContent="Correct Number"
-            document.querySelector('.score').textContent=Score
-            document.querySelector('body').style.backgroundColor='60b347'
-            localStorage.setItem("HighScore",Score)
-            document.querySelector('.HighScore').textContent=Score
+        else if (guess == Snumber) {
+            document.querySelector('.message').textContent = "Correct Number"
+            // document.querySelector('.score').textContent=Score
+            document.querySelector('body').style.backgroundColor = '60b347'
+            if (Score > OldScore) {
+                localStorage.setItem("HighScore", Score)
+            }
+            document.querySelector('.HighScore').textContent = Score
+            document.querySelector('.number').textContent = "Correct Guess"
         }
-    else if(guess>Snumber)
-        {
-            document.querySelector('.message').textContent="Too High"
+        else if (guess > Snumber) {
+            document.querySelector('.message').textContent = "Too High"
             Score--
-            if(Score==0)
-            {
-                document.querySelector('.message').textContent="Game Over"
-                document.querySelector('.score').textContent=Score
+            if (Score == 0) {
+                document.querySelector('.message').textContent = "Game Over"
+                document.querySelector('.score').textContent = Score
             }
-            else
-            {
-                document.querySelector('.score').textContent=Score
+            else {
+                document.querySelector('.score').textContent = Score
             }
 
         }
-    else if(guess<Snumber)
-        {
-             document.querySelector('.message').textContent="Too Low"
-             Score--
-             if(Score==0)
-             {
-                 document.querySelector('.message').textContent="Game Over"
-                 document.querySelector('.score').textContent=Score
-             }
-             else
-             {
-                document.querySelector('.score').textContent=Score
-             }
-    
+        else if (guess < Snumber) {
+            document.querySelector('.message').textContent = "Too Low"
+            Score--
+            if (Score == 0) {
+                document.querySelector('.message').textContent = "Game Over"
+                document.querySelector('.score').textContent = Score
+            }
+            else {
+                document.querySelector('.score').textContent = Score
+            }
+
 
         }
-        document.querySelector('.guess').value=''
-})
+        document.querySelector('.guess').value = ''
+    })
 document.querySelector('.again').addEventListener('click',
 
-function()
-{
-    document.querySelector('.guess').value=''
-    document.querySelector('.message').textContent="Start Guessing...."
-    document.querySelector('.score').textContent=20
-    document.querySelector('body').style.backgroundColor='white'
-    
-    Snumber=Math.trunc(Math.random()*20)+1
-    console.log(Snumber)
+    function () {
+        document.querySelector('.guess').value = ''
+        document.querySelector('.message').textContent = "Start Guessing...."
+        document.querySelector('.score').textContent = 20
+        document.querySelector('body').style.backgroundColor = 'white'
+        document.querySelector('.number').textContent = "Guess me"
+        Snumber = Math.trunc(Math.random() * 20) + 1
+        OldScore = localStorage.getItem('HighScore', Score)
+        console.log(Snumber)
 
-})
+    })
